34e6c7cb208ad56505a2c4294eecae3f7c628f9f
yousefgg <44702055+yousefgg@users.noreply.github.com>
2018-11-12 20:10:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,17 @@
-﻿const Discord = require('discord.js');
+ const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "!";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
 });
 
+
 client.on('ready', async() => {
 var server = "510399878002901002"; // ايدي السررفر
 var channel = "510399878002901005";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , **')
     },305);
 })
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
- 
-client.login(process.env.BOT_TOKEN);
+client.login(توكن حسابك); 
