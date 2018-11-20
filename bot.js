d0377f27035247776a34a25cbfb7054049b92b10
yousefgg <44702055+yousefgg@users.noreply.github.com>
2018-11-20 10:28:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
  const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = "1";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
 });
-
 
 client.on('ready', async() => {
 var server = "510399878002901002"; // ايدي السررفر
@@ -14,4 +14,21 @@ var channel = "510399878002901005";//ايدي الروم
     },305);
 })
 
-client.login(process.env.BOT_TOKEN); 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
+client.login(process.env.BOT_TOKE
